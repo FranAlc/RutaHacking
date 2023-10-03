12e91c49e7ac9050b0bb7b49ce6e4f202d2a6223
Franco Galan <99420835+FranAlc@users.noreply.github.com>
2023-10-03 12:49:02 -0300

    Aclaración de lo realizado en la imagen ReverseSh

---
+++ Hacking/Maquinas Virtuales/VM Metasploit2/puerto 21 FTP/-reverseShell-python-serverTelnet/aclaracion.py
@@ -0,0 +1,13 @@
+"""
+Codigo e explicacion:
+
+El Codigo que utilice esta en mi otro repositorio "ethical-hacking" o en la carpeta "exploit_ftp_codigo_python" de este mismo repo.
+Implemente una forma de escribir distintos USERs y PASS, hasta que se lograra ingresar dentro del puerto.
+Luego realice una reverse shell utilizando TELNET y intente que se me devolviera un LIST de msf2 a mi maquina.
+
+The code that is used is in my other repository "ethical-hacking" or in the "exploit_ftp_codigo_python" folder of this same repository.
+What I implemented in my code was that you can type different USERS and PASSWORDS until you enter the port.
+I then did a reverse shell using TELNET and tried to get a LIST from msf2 and return it to my machine.
+"""
+
+#Intento seguir mejorando y crear un payload que lo haga por si mismo una vez dentro.
